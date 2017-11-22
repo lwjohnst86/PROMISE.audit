76d7833fa7112f1353d8cac8161f7036fd18f4ad
Luke W Johnston <lwjohnst@gmail.com>
2017-11-22 15:33:27 -0500

    added in_set check function (plus minor edits to other docs)

--- R/number_value_checks.R
+++ R/number_value_checks.R
@@ -1,6 +1,6 @@
 #' Audits column values within a certain numeric range.
 #'
-#' Uses the [`assertr::within_bounds`].
+#' Uses the [`assertr::within_bounds`] function.
 #'
 #' @param .data Data to audit.
 #' @param .low Low value in the range.
@@ -12,13 +12,42 @@
 #'
 #' @examples
 #' library(magrittr)
-#' flaws <- data.frame(a = rnorm(10)) %>%
+#' ds <- data.frame(a = rnorm(10), b = rnorm(10))
+#'
+#' flaws <- ds %>%
 #' chk_in_range(-1, 1, "a")
+#' flaws
 #' attributes(flaws)$assertr_errors
+#'
 chk_in_range <- function(.data, .low, .high, .variables) {
     chk_assert_func(
         .data = .data,
         .function = assertr::within_bounds(.low, .high),
+        .variables = .variables
+    )
+}
+
+#' Audits column values of specific integers.
+#'
+#' Uses the [`assertr::in_set`] function.
+#'
+#' @inherit chk_in_range return params
+#' @param .values The integer values.
+#'
+#' @export
+#'
+#' @examples
+#' library(magrittr)
+#' ds <- data.frame(a = 1:3)
+#'
+#' flaws <- ds %>%
+#' chk_in_set(1:2, "a")
+#' flaws
+#' attributes(flaws)$assertr_errors
+chk_in_set <- function(.data, .values, .variables) {
+    chk_assert_func(
+        .data = .data,
+        .function = assertr::in_set(.values),
         .variables = .variables
     )
 }
