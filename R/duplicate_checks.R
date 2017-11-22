4ee2a66319f32fa84d49ddd16a29fadfba5706f3
Luke W Johnston <lwjohnst@gmail.com>
2017-11-22 16:30:51 -0500

    function to check for duplicates

---
+++ R/duplicate_checks.R
@@ -0,0 +1,45 @@
+#' Audits values in columns for duplicate values.
+#'
+#' Among other things, uses [`assertr::is_uniq`].
+#'
+#' @inherit chk_in_range return params
+#'
+#' @export
+#'
+#' @examples
+#' library(magrittr)
+#'
+#' # dataset with one duplicate value between id and time
+#' ds <- data.frame(
+#' id = c(rep(1:3, each = 2), 3),
+#' time = c(rep(1:2, times = 3), 1)
+#' )
+#' ds
+#'
+#' flaws <- chk_duplicate(ds, c("id", "time"))
+#' attributes(flaws)$assertr_errors # Should be one
+#'
+#' flaws <- chk_duplicate(ds, c("id"))
+#' attributes(flaws)$assertr_errors # Should be all of them.
+chk_duplicate <- function(.data, .variables) {
+    stopifnot(all(.variables %in% names(.data)))
+
+    if (length(.variables) > 1) {
+        column <- paste0(.variables, collapse = "--")
+        .data[column] <- apply(.data[, .variables], 1, paste0, collapse = "-")
+    } else {
+        column <- .variables
+    }
+
+    flaws_chk <- chk_assert_func(
+        .data = .data,
+        .function = assertr::is_uniq,
+        .variables = column
+    )
+
+    if (length(.variables) > 1) {
+        flaws_chk[column] <- NULL
+    }
+
+    return(flaws_chk)
+}
