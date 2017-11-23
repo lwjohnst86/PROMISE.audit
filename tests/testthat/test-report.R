4bf102d32dadabaa23e70f9038442491abae7499
Luke W Johnston <lwjohnst@gmail.com>
2017-11-22 20:18:31 -0500

    unit testing of audit report

---
+++ tests/testthat/test-report.R
@@ -0,0 +1,24 @@
+context("Audit report")
+
+n <- 5
+testdata <- data.frame(
+    Hours = c(sample(1:24, n), 25),
+    Days = c(sample(1:7, n), 8),
+    Months = c(sample(1:12, n), 13),
+    Temp = c(runif(n, -30, 30), 2000),
+    Latitude = c(runif(n, 0, 90), 100),
+    Longitude = c(runif(n, 0, 180), 190)
+)
+
+test_that("Audit report generated", {
+    audit <- chk_in_range(testdata, 0, Inf, "Latitude")
+    expect_message(aud_report(audit), "found no flaws")
+
+    audit <- chk_in_range(audit, 0, 24, "Hours")
+    audit <- chk_in_set(audit, 0:7, "Days")
+    expect_message(aud_report(audit), "column (Hours|Days)")
+    expect_message(aud_report(audit), "violations: 1")
+    expect_message(aud_report(audit), "values: (25|8)")
+    expect_message(aud_report(audit, location = TRUE), "row numbers: *6")
+})
+
