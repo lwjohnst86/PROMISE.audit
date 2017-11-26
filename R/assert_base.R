daa6a7c6250bd8407a055dc0cad31f6ca50b86a2
Luke W Johnston <lwjohnst@gmail.com>
2017-11-26 15:03:29 -0500

    added insist fun plus minor doc fixes

--- R/assert_base.R
+++ R/assert_base.R
@@ -1,27 +1,46 @@
 
 #' General data audit function to create other specific audit functions.
 #'
-#' Thin wrapper around the [`assertr::assert_`] function.
+#' Thin wrapper around the [assertr::assert_()] and [assertr::insist_()] functions.
 #'
+#' @name assert_base
 #' @param .data Data to check for errors.
-#' @param .function `assertr` function to apply to the `assertr::assert` command.
+#' @param .function `assertr` function to apply to the [assertr::assert()] command.
 #' @param .variables The columns to run the audit checks on.
 #'
 #' @return Adds `assertr_errors` to data.frame.
-#' @seealso [`assertr::assert`]
-#' @export
+#' @seealso See documentation on either [assertr::assert()] or [assertr::insist()]
 #'
 #' @examples
 #' library(magrittr)
 #' library(assertr)
+#'
 #' flaws <- iris %>%
 #' chk_assert_func(within_bounds(5, 7), "Sepal.Length")
-#' attributes(flaws)$assertr_errors
+#' aud_report(flaws)
 #'
+#' flaws <- iris %>%
+#' chk_insist_func(within_n_mads(1), "Sepal.Length")
+#' aud_report(flaws)
+#'
+NULL
+
+#' @rdname assert_base
 chk_assert_func <- function(.data, .function, .variables) {
     assertr::assert_(
         data = .data,
         predicate = .function,
+        .dots = .variables,
+        success_fun = assertr::success_continue,
+        error_fun = assertr::error_append
+    )
+}
+
+#' @rdname assert_base
+chk_insist_func <- function(.data, .function, .variables) {
+    assertr::insist_(
+        data = .data,
+        predicate_generator = .function,
         .dots = .variables,
         success_fun = assertr::success_continue,
         error_fun = assertr::error_append
